25221ac69f33ac791e5ea38f962927a64e92ade4
yoosanghun <d0296989@gmail.com>
2023-09-27 14:54:28 +0900

    add init.sh

---
+++ init.sh
@@ -0,0 +1,6 @@
+wget https://github.com/HealthOverView/yolov5_for_gosari/archive/refs/heads/main.zip -O yolo.zip
+unzip yolo.zip -d ./
+mv ./yolov5_for_gosari-main/* ./
+rm -rf yolo.zip
+rm -rf yolov5_for_gosari-main
+pip install -r requirements.txt
